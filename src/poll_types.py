9c4d4ed2515747a5d9e54aca7b163594359e44e0
Jacotsu <Jacotsu@users.noreply.github.com>
2020-08-28 01:03:03 +0200

    Added admin poll types

---
+++ src/poll_types.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#
+# anon_chat_bot is a telegram bot whose main function is to manage an
+# anonymous chat lounge
+# Copyright (C) <2020>  <jacotsu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from enum import IntEnum, auto, unique
+import utils
+
+
+@unique
+class PollTypes(IntEnum,
+                metaclass=utils.CustomEnumMetaForCaseInsensiviSubscript):
+    SET_DEFAULT_PERMISSIONS = auto()
+    SET_USER_PERMISSIONS = auto()
+    SET_ROLE_PERMISSIONS = auto()
+    SET_DEFAULT_ROLE = auto()
