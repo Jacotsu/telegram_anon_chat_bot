a43ee72fa5a27a1c8a37eb04b2d95ddc6e3820bc
Jacotsu <Jacotsu@users.noreply.github.com>
2020-08-28 01:14:20 +0200

    Implemented username to user_id resolver

---
+++ src/user_resolver.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#
+# anon_chat_bot is a telegram bot whose main function is to manage an
+# anonymous chat lounge
+# Copyright (C) <2020>  <jacotsu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import logging
+from telegram import Update, Message
+from telethon.sync import TelegramClient
+from telethon.tl.functions.users import GetFullUserRequest
+from utils import SingletonDecorator
+
+logger = logging.getLogger(__name__)
+
+
+class UserResolverError(Exception):
+    pass
+
+
+@SingletonDecorator
+class UserResolver:
+    def __init__(self, config):
+        api_id = config["UsernameResolver"]["ApiId"]
+        api_hash = config["UsernameResolver"]["ApiHash"]
+
+        if api_id and api_hash:
+            self._tg_client = TelegramClient('anon chat bot user resolver',
+                                             api_id, api_hash)
+            logger.debug('Initialized user resolver with username support')
+        else:
+            self._tg_client = None
+
+    def acquire_target_user_from_cmd(self, update, target_position=1):
+        '''
+        If update is a reply to a message the sender of the original message
+        is returned, otherwise it takes the text at the specified position
+        and tries to resolve it if it's a username
+        '''
+        replied_msg = update.message.reply_to_message
+        split_cmd = update.message.text.split()
+        split_cmd_len = len(split_cmd)
+
+        if replied_msg:
+            return replied_msg.from_user.id
+        else:
+            if split_cmd_len > target_position:
+                return self.resolve(split_cmd[target_position])
+            else:
+                raise ValueError('The command string is too short to contain'
+                                 'the target word '
+                                 f'(target_position={target_position})')
+
+    def resolve(self, value_or_update) -> int:
+        if type(value_or_update) == int:
+            return value_or_update
+        elif type(value_or_update) == str:
+            if self._tg_client:
+                with self._tg_client as tg_client:
+                    peer_id = tg_client.get_peer_id(value_or_update)
+                    if peer_id:
+                        return peer_id
+                    else:
+                        raise ValueError(f'{value_or_update} is an '
+                                         'invalid username')
+            else:
+                raise UserResolverError('Username resolution is not '
+                                        'configured')
+        elif type(value_or_update) == Update:
+            return value_or_update.message.from_user.id
+        elif type(value_or_update) == Message:
+            return value_or_update.from_user.id
+        else:
+            raise ValueError(f'{value_or_update}\'s type is wrong')
+
+    def get_user_info(self, username_or_id):
+        if self._tg_client:
+            with self._tg_client as tg_client:
+                info = tg_client(GetFullUserRequest(username_or_id))
+                return {
+                    'first_name': info.user.first_name,
+                    'last_name': info.user.last_name,
+                    'username': info.user.username,
+                    'id': info.user.id
+                }
+        else:
+            raise UserResolverError('Username resolution is not '
+                                    'configured')
