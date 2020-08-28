e2a96b4e4e7c3526a48faa6d9f463b13982ecbe8
Jacotsu <Jacotsu@users.noreply.github.com>
2020-08-28 18:16:31 +0200

    Cleaned up code

--- src/security.py
+++ src/security.py
@@ -27,86 +27,20 @@ from utils import user_log_str
 logger = logging.getLogger(__name__)
 
 
-def execute_if_hierarchy_is_respected(
+def is_hierarchy_respected(
     agent: User,
-    receiver: User,
-    function,
-    update: Update,
-    agent_message_on_success: str = None,
-    agent_message_on_failure: str = None,
-    log_message_on_success: str = None,
-    log_message_on_failure: str = None,
-    callback_on_success = None,
-    callback_on_failure = None
+    target: User
 ):
 
-    if receiver.role.power < agent.role.power:
-        function()
-        if agent_message_on_success:
-            update.message.reply_text(agent_message_on_success)
-        if log_message_on_success:
-            logger.info(log_message_on_success)
-        if callback_on_success:
-            callback_on_success()
-    else:
-        if agent_message_on_failure:
-            update.message.reply_text(agent_message_on_failure)
-        if log_message_on_failure:
-            logger.warning(log_message_on_failure)
-        if callback_on_failure:
-            callback_on_failure()
+    if target.role.power < agent.role.power:
+        return True
+    return False
 
-def execute_if_role_hierarchy_is_preserved(
+def is_role_hierarchy_respected(
     agent: User,
-    target_role: Role,
-    function,
-    update: Update,
-    agent_message_on_success: str = None,
-    log_message_on_success: str = None,
-    callback_on_success = None,
-    callback_on_failure = None
+    target_role: Role
 ):
     if agent.role.power > target_role.power:
         if target_role.permissions in agent.permissions:
-            function()
-            if callback_on_success:
-                callback_on_success()
-        else:
-            update.message.reply_text(
-                'You can\'t set a role whose permissions are '
-                'more than yours'
-            )
-            logger.warning(
-                f'{user_log_str(update)} has tried to set a role '
-                'whose permissions are higher than his'
-            )
-    else:
-        update.message.reply_text(
-            'You can\'t set a role whose power is higher '
-            'than yours as default'
-        )
-        logger.warning(
-            f'{user_log_str(update)} has tried to set a role '
-            'whose power is higher than his as default role'
-        )
-
-    if callback_on_failure:
-        callback_on_failure()
-
-
-
-def has_permissions(logger=logging.getLogger(__name__)):
-    def wrap(f):
-        def wrapped_f(*args, **kwargs):
-            f(*args, **kwargs)
-            try:
-                user = args[0]['message'].from_user
-                data = args[0]['message'].text
-            except AttributeError:
-                user = args[0]['callback_query'].from_user
-                data = args[0]['callback_query'].data
-
-            logger.info(f"{user['username']} ({user['id']}) Has executed: "
-                        f"{f.__name__} \"{data}\"")
-        return wrapped_f
-    return wrap
+            return True
+    return False
